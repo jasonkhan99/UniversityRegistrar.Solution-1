9bc517fe71ff4c94a103bff00f1395481e18099e
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-05 13:59:35 -0700

    finish student views and controller.
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>
    Co-authored-by: Kyle Hubbard  <kyle.james.hubbard@gmail.com>

--- Registrar/Controllers/StudentController.cs
+++ Registrar/Controllers/StudentController.cs
@@ -43,7 +43,24 @@ namespace Registrar.Controllers
         .FirstOrDefault(student => student.StudentId == id);
       return View(thisStudent);
     }
+    public ActionResult Edit(int id) 
+    {
+      var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+      return View(thisStudent);
+    }
 
+    [HttpPost]
+    public ActionResult Edit(Student student, int CourseId)
+    {
+      if (CourseId != 0)
+      {
+        _db.CourseStudent.Add(new CourseStudent() {CourseId = CourseId, StudentId = student.StudentId});
+      }
+      _db.Entry(student).State = EntityState.Modified; 
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
     public ActionResult Delete(int id)
     {
       var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
